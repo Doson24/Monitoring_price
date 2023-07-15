9fbc6902c0a9ce52b33589adca171b1c7627a074
Doson <karbushev-1991@mail.ru>
2023-07-15 14:03:55 +0700

    добавил выбор города доставки

--- Price_item/Ozon.py
+++ Price_item/Ozon.py
@@ -10,6 +10,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from save_DB import save_db
+from selenium.common.exceptions import TimeoutException
 
 
 @dataclass
@@ -20,13 +21,18 @@ class Card:
     rewiews: int
     # sold: int
     # catalog: str
-    date_create: str
+    date_create: str = datetime.today().strftime("%d-%m-%Y")
 
 
 def get_one_card_data(driver, base_url):
-    name = WebDriverWait(driver, 30). \
-        until(EC.presence_of_element_located((
-        (By.XPATH, ".//*[@data-widget='webProductHeading']"))))
+    try:
+        name = WebDriverWait(driver, 30). \
+            until(EC.presence_of_element_located((
+            (By.XPATH, ".//div[@data-widget='webProductHeading']"))))
+    except TimeoutException:
+        sold_out = driver.find_element(By.CLASS_NAME, 'kv8').text
+        name = driver.find_element(By.CLASS_NAME, 'k5v')
+        return Card(name.text, base_url, sold_out, sold_out)
     # name = driver.find_element(By.XPATH, ".//*[@data-widget='webProductHeading']").text
 
     layout = driver.find_elements(By.XPATH, ".//*[@data-widget='webReviewProductScore']")[0].text
@@ -39,9 +45,7 @@ def get_one_card_data(driver, base_url):
         cost_ozon = driver.find_element(By.XPATH, ".//*[@data-widget='webPrice']").text
     cost_ozon_clen = cost_ozon.replace('\u2009', '').split('₽')[0]
 
-    date_create = datetime.today().strftime("%d-%m-%Y")
-
-    return Card(name.text, base_url, int(cost_ozon_clen), rewiews, date_create)
+    return Card(name.text, base_url, int(cost_ozon_clen), rewiews)
 
 
 def save_file(data):
@@ -52,8 +56,32 @@ def save_file(data):
     print('>>>Сохранение завершено<<<')
 
 
+def setup_city(driver):
+    driver.get('https://www.ozon.ru/')
+    button_city = driver.find_elements(By.XPATH, './/button[@tabindex="0"]')[2]
+    if button_city.text == 'Укажите адрес доставки':
+        button_city.click()
+        time.sleep(1)
+        WebDriverWait(driver, 30).until(EC.presence_of_element_located(
+            (By.CLASS_NAME, 'r6d'))).click()
+        # driver.find_element(By.CLASS_NAME, 'r6d').click()
+        time.sleep(1)
+        WebDriverWait(driver, 30).until(EC.presence_of_element_located(
+            (By.XPATH, './/input[@type="search"]'))).send_keys('Железногорск')
+        # driver.find_element(By.XPATH, './/input[@type="search"]').send_keys('Железногорск')
+        time.sleep(1)
+        WebDriverWait(driver, 30).until(EC.presence_of_element_located(
+            (By.CLASS_NAME, 'dr0'))).click() # Клик на Железногорск из списка
+        # driver.find_elements(By.CLASS_NAME, 'dr0')[0].click()
+    else:
+        raise Exception('Изменился путь к изменению Адреса доставки')
+
+
 def urls_monitoring(driver) -> pd.DataFrame:
     urls = [
+        'https://www.ozon.ru/product/polnoratsionnyy-suhoy-korm-s-govyadinoy-black-angus-dlya-domashnih-koshek-starshe-1-goda-1-2-kg-431298791/?oos_search=false',
+        'https://www.ozon.ru/product/suhoy-korm-dlya-koshek-purina-one-adult-s-govyadinoy-s-tselnymi-zlakami-750-g-137590849',
+        'https://www.ozon.ru/product/suhoy-korm-dlya-koshek-purina-one-s-vysokim-soderzhaniem-kuritsy-i-tselnymi-zlakami-9-75-kg-689850724',
         'https://www.ozon.ru/product/shef-nozh-dlya-narezki-myasa-ryby-ovoshchey-i-fruktov-kuhonnyy-nozh-povarskoy-nozh-dlya-kuhni-samura-163145300',
         'https://www.ozon.ru/product/generator-benzinovyy-portativnyy-lavada-g4000-elektrogenerator-benzoelektrostantsiya-3-3-3-5-819332872/?sh=1RJHcmpPdA',
         'https://www.ozon.ru/product/nabor-instrumentov-dlya-chistki-ushey-7-predmetov-instrument-dlya-chistki-ushey-526254914/?sh=1RJHcmMATA,'
@@ -63,6 +91,8 @@ def urls_monitoring(driver) -> pd.DataFrame:
         'https://www.ozon.ru/product/nozh-santoku-dlya-narezki-myasa-ryby-ovoshchey-i-fruktov-yaponskiy-kuhonnyy-nozh-povarskoy-shef-163145302/?from_sku=163145300&oos_search=false&sh=1RJHcrl_Og',
 
     ]
+    setup_city(driver)
+
     cards = []
     for url in urls:
         driver.get(url)
@@ -81,7 +111,7 @@ if __name__ == '__main__':
 
     data = urls_monitoring(driver)
     save_db(data,
-            name_db='C:\\Users\\user\\Desktop\\Projects\\Price_monitoring\\Price_item\\bat\\online_markets.db',
+            path='C:\\Users\\user\\Desktop\\Projects\\Price_monitoring\\Price_item\\bat\\online_markets.db',
             table_name='Ozon')
 
     driver.quit()
