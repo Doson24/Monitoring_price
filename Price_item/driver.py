f96b5516587ffb6f3d85b2e6dfab06604ce36f53
Doson <karbushev-1991@mail.ru>
2023-09-05 00:40:00 +0700

    замена загрузчика драйвера

--- Price_item/driver.py
+++ Price_item/driver.py
@@ -7,6 +7,8 @@ from webdriver_manager.chrome import ChromeDriverManager
 import undetected_chromedriver as uc
 from selenium.webdriver.chrome.service import Service as ChromeService
 
+from chrome_driver_downloder import get_chromedriver_fp
+
 
 def init_webdriver(headless=True):
     chrome_options = Options()
@@ -50,11 +52,11 @@ def init_webdriver(headless=True):
     chrome_options.add_argument(f"user-agent={ua_random}")
     """
 
-    driver = webdriver.Chrome(
-        executable_path=r'C:\install\chromedriver.exe',
-        # browser_executable_path=r'C:\install\chrome-win64\chrome.exe',
-        # executable_path=ChromeDriverManager(
-        #        latest_release_url='https://edgedl.me.gvt1.com/edgedl/chrome/chrome-for-testing/116.0.5845.96/win64/chromedriver-win64.zip').install(),
-        options=chrome_options,
-    )
+    driver = uc.Chrome(driver_executable_path=get_chromedriver_fp(),
+                       # executable_path=r'C:\install\chromedriver.exe',
+                       # browser_executable_path=r'C:\install\chrome-win64\chrome.exe',
+                       # executable_path=ChromeDriverManager(
+                       #        latest_release_url='https://edgedl.me.gvt1.com/edgedl/chrome/chrome-for-testing/116.0.5845.96/win64/chromedriver-win64.zip').install(),
+                       options=chrome_options,
+                       )
     return driver
